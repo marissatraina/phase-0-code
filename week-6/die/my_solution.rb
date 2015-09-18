7c3355e4f3767421c22489b9412dd3235f34cf10
Maria Piper <maria@devbootcamp.com>
2015-09-18 13:49:49 -0700

    Update release numbers

--- week-6/die/my_solution.rb
+++ week-6/die/my_solution.rb
@@ -4,14 +4,14 @@
 # I worked on this challenge [by myself, with: ].
 # I spent [#] hours on this challenge.
 
-# 2. Pseudocode
+# Pseudocode
 
 # Input:
 # Output:
 # Steps:
 
 
-# 3. Initial Solution
+# Initial Solution
 
 class Die
   def initialize(labels)
@@ -26,18 +26,13 @@ end
 
 
 
-# 4. Refactored Solution
+# Refactored Solution
 
 
 
 
 
 
-# 1. DRIVER TESTS GO BELOW THIS LINE
 
 
-
-
-
-
-# 5. Reflection
+# Reflection
