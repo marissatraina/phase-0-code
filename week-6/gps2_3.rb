a5b3e0a8a26d2510269bc6d9aca00d120035e665
Lucas Willett <me@ltw.io>
2015-11-25 18:18:55 -0600

    Rename variables to aid in understanding.

--- week-6/gps2_3.rb
+++ week-6/gps2_3.rb
@@ -6,13 +6,13 @@
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
+def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
   error_counter = 3
 
   library.each do |food|
     if library[food] != library[item_to_make]
-      p error_counter += -1
+      error_counter += -1
     end
   end
 
@@ -21,13 +21,13 @@ def serving_size_calc(item_to_make, order_quantity)
   end
 
   serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
+  remaining_ingredients = num_of_ingredients % serving_size
 
-  case serving_size_mod
+  case remaining_ingredients
   when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
   end
 end
 
